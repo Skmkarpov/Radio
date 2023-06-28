6a092a940d869952bdf68a0a318c871a2b0804a4
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-06-28 20:45:42 +0300

    Первая итерация

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Radio {
+public class Radio {
+
+    private final int minChannelNumber = 0;
+    private final int maxChannelNumber = 9;
+    private final int maxSoundLevel = 10;
+    private final int minSoundLevel = 0;
+
+    private int currentChannel = 0;
+    private int currentLoud = 0;
+
+    public void setNextChannel() {
+        if (currentChannel < maxChannelNumber) {
+            currentChannel++;
+        } else {
+            currentChannel = minChannelNumber;
+        }
+
+    }
+
+    public void setPreviousChannel() {
+        if (currentChannel > minChannelNumber) {
+            currentChannel--;
+        } else {
+            currentChannel = maxChannelNumber;
+        }
+
+    }
+
+
+    public void setChannel(int myChannel) {
+        if ((myChannel >= minChannelNumber) & (myChannel <= maxChannelNumber)) {
+            currentChannel = myChannel;
+        }
+
+    }
+
+    public void setMoreLoud() {
+        if (currentLoud < maxSoundLevel) {
+            currentLoud++;
+        }
+
+    }
+
+    public void setLessLoud() {
+        if (currentLoud > minSoundLevel) {
+            currentLoud--;
+        }
+
+    }
+
+    public void setSoundLevel(int mySound) {
+        if ((mySound >= minSoundLevel) & (mySound <= maxSoundLevel)) {
+            currentLoud = mySound;
+        }
+
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+
+    }
+
+    public int getCurrentLoud() {
+        return currentLoud;
+
+    }
+
+
 }
--- src/test/java/TestRadio.java
+++ src/test/java/TestRadio.java
@@ -1,2 +1,116 @@
-package PACKAGE_NAME;public class TestRadio {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestRadio {
+
+    @Test
+    void testSetNextChannelMiddle() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(5);
+        myRadio.setNextChannel();
+        int actual = myRadio.getCurrentChannel();
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetNextChannelMax() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(9);
+        myRadio.setNextChannel();
+        int actual = myRadio.getCurrentChannel();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetPreviousChannelMiddle() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(5);
+        myRadio.setPreviousChannel();
+        int actual = myRadio.getCurrentChannel();
+        int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetPreviousChannelMin() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(0);
+        myRadio.setPreviousChannel();
+        int actual = myRadio.getCurrentChannel();
+        int expected = 9;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetChannelCorrect() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(5);
+        int actual = myRadio.getCurrentChannel();
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetChannelMoreThanMax() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(3);
+        myRadio.setChannel(10);
+        int actual = myRadio.getCurrentChannel();
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetChannelLessThanMin() {
+        Radio myRadio = new Radio();
+        myRadio.setChannel(5);
+        myRadio.setChannel(-1);
+        int actual = myRadio.getCurrentChannel();
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetMoreSound() {
+        Radio myRadio = new Radio();
+        myRadio.setSoundLevel(5);
+        myRadio.setMoreLoud();
+        int actual = myRadio.getCurrentLoud();
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetLessSound() {
+        Radio myRadio = new Radio();
+        myRadio.setSoundLevel(5);
+        myRadio.setLessLoud();
+        int actual = myRadio.getCurrentLoud();
+        int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetLessThanMinSound() {
+        Radio myRadio = new Radio();
+        myRadio.setSoundLevel(0);
+        myRadio.setLessLoud();
+        int actual = myRadio.getCurrentLoud();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetMoreThanMaxSound() {
+        Radio myRadio = new Radio();
+        myRadio.setSoundLevel(10);
+        myRadio.setMoreLoud();
+        int actual = myRadio.getCurrentLoud();
+        int expected = 10;
+        assertEquals(expected, actual);
+    }
+
 }
